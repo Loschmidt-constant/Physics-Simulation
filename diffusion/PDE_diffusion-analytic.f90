ec8fadcadacd1fbe91a823e9b9d0e4d11144a085
Loschmidt's constant <B17T1071Z@edu.tottori-u.ac.jp>
2019-11-10 01:22:00 +0900

    途中経過
    まだ完成していない
    
    解析解が正しくないと思われる.

--- diffusion/PDE_diffusion-analytic.f90
+++ diffusion/PDE_diffusion-analytic.f90
@@ -6,8 +6,10 @@ implicit none
 integer,parameter		:: n_max = 1000
 integer,parameter		:: m_max = 10
 
-real(4),parameter		:: x_min = 0.0
-real(4),parameter		:: x_max = 1.0
+real(8)	,parameter		:: pi = 4d0*atan(1d0)
+
+real(4),parameter			:: x_min = 0.0
+real(4),parameter			:: x_max = 1.0
 real(8),parameter 		:: dx   = (x_max -x_min)/dble(m_max)	!! 空間刻み幅
 real(8),parameter 		:: dt   = 1.0/dble(n_max)						!! 時間刻み幅
 
@@ -33,49 +35,39 @@ use set_functions
 
 implicit none
 
-integer										:: i
-integer										:: j
+integer									:: i
+integer									:: j
 real,dimension(0:m_max)		:: u
 real,dimension(0:m_max)		:: x
-real,dimension(0:n_max)			:: t
+real											:: t
 character(len=50) :: filename
 character(len=50) :: tmp
-real lambda
-
-lambda = kappa * (dt/(dx**2))	!! 刻み幅定数
- 
-u(0) = 0.0
-u(m_max) = 10.0
 
 do i = 0, m_max
  x(i) = dx * i
 end do
 
-if (lambda <= 0.5) then
+u(0) = 0.0
+u(m_max) = 10.0
 
-	call get_ICs(u)
+call get_ICs(u)
 
-	do j = 1, n_max+1
-		t = dt * j
-		call calculation_unew(u)
-		if (mod(j-1,100)==0) then
-			write(tmp,'(i4)') j-1 
-			filename='diffusion_'//trim(adjustl(tmp))//'.dat'
-			open(10, file = filename, status="replace")
-			
-			do i = 0, m_max
-				write(10,*) x(i), u(i)
-			end do
+do j = 0, n_max
+	t = dt * j
+	call calculation_unew(u,x,t)	
+	if (mod(j,100)==0) then
+		write(tmp,'(i4)') j 
+		filename='diffusion-ana_'//trim(adjustl(tmp))//'.dat'
+		open(10, file = filename, status="replace")
 		
-			close(10)
+		do i = 0, m_max
+			write(10,*) x(i), u(i)
+		end do
 		
-		end if
-	end do
-
-else
-	write(*,*) "この拡散数は Von Neumann の安定条件を満たしていない."
-
-end if
+		close(10)
+		
+	end if
+end do
 
 stop
 end program main
@@ -88,7 +80,7 @@ use constant_values
 use set_functions
 
 implicit none
-integer									:: i
+integer								:: i
 real										:: x
 real,dimension(0:m_max)	:: u
 
@@ -109,26 +101,29 @@ end subroutine get_ICs
 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-subroutine calculation_unew(u)
+subroutine calculation_unew(u,x,t)
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 use constant_values
 use set_functions
 
 implicit none
-integer			  						:: i
+integer			  					:: i
 real,dimension(0:m_max)	:: u
 real,dimension(0:m_max)	:: u_old
-real lambda
-
-lambda = kappa * (dt/(dx**2))	!! 刻み幅定数
+real,dimension(0:m_max)	:: x
+real,dimension(0:m_max)	:: c
+real										:: t
 
 do i = 0, m_max
 	u_old(i) = u(i)
 end do
 
-do i = 1, m_max-1
-	u(i) = u_old(i) + lambda * (u_old(i+1) - 2*u_old(i) + u_old(i-1))
+do i = 1, m_max
+			c(i) = -(20/(pi*i))*(cos(pi*i*x(i))-cos(0.5*pi*i*x(i)))
+			u(i) = u_old(i-1) + c(i)*exp(-0.5*t*(pi*i)**2)*sin(pi*i*x(i))
 end do
 
+!write(*,*) u(1)
 
 end subroutine calculation_unew
+
